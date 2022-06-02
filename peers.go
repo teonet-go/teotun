510cc4ff90cfd69d9e20c646b12c2de567ef6af5
Kirill Scherba <kirill@scherba.ru>
2022-06-02 13:01:10 +0300

    Peers module created

---
+++ peers.go
@@ -0,0 +1,54 @@
+// Copyright 2022 Kirill Scherba <kirill@scherba.ru>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Teotun is package. Peers manage module.
+
+package teotun
+
+import "sync"
+
+// peers is tunnels connected peers structure and methods receiver
+type peers struct {
+	*sync.RWMutex
+	m map[string]interface{}
+}
+
+// newPeers crete new peers object
+func (t *Teotun) newPeers() (p *peers) {
+	p = new(peers)
+	p.RWMutex = new(sync.RWMutex)
+	p.m = make(map[string]interface{})
+	return
+}
+
+// add peer
+func (p *peers) add(address string) {
+	p.Lock()
+	defer p.Unlock()
+	p.m[address] = nil
+}
+
+// del peer
+func (p *peers) del(address string) {
+	p.Lock()
+	defer p.Unlock()
+	delete(p.m, address)
+}
+
+// get peer
+func (p *peers) get(address string) (v interface{}, ok bool) {
+	p.RLock()
+	defer p.RUnlock()
+	v, ok = p.m[address]
+	return
+}
+
+// forEach calls function f for each added peer
+func (p *peers) forEach(f func(address string)) {
+	p.RLock()
+	defer p.RUnlock()
+	for address := range p.m {
+		f(address)
+	}
+}
--- teotun.go
+++ teotun.go
@@ -10,7 +10,6 @@ import (
 	"errors"
 	"os/exec"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/songgao/packets/ethernet"
@@ -214,49 +213,4 @@ func (t *Teotun) postConnect(command string) (err error) {
 	out, err := exec.Command(com[0], arg...).Output()
 	t.log.Debug.Printf("\n%s\n", out)
 	return
-}
-
-// peers is tunnels connected peers structure and methods receiver
-type peers struct {
-	*sync.RWMutex
-	m map[string]interface{}
-}
-
-// newPeers crete new peers object
-func (t *Teotun) newPeers() (p *peers) {
-	p = new(peers)
-	p.RWMutex = new(sync.RWMutex)
-	p.m = make(map[string]interface{})
-	return
-}
-
-// add peer
-func (p *peers) add(address string) {
-	p.Lock()
-	defer p.Unlock()
-	p.m[address] = nil
-}
-
-// del peer
-func (p *peers) del(address string) {
-	p.Lock()
-	defer p.Unlock()
-	delete(p.m, address)
-}
-
-// get peer
-func (p *peers) get(address string) (v interface{}, ok bool) {
-	p.RLock()
-	defer p.RUnlock()
-	v, ok = p.m[address]
-	return
-}
-
-// forEach calls function f for each added peer
-func (p *peers) forEach(f func(address string)) {
-	p.RLock()
-	defer p.RUnlock()
-	for address := range p.m {
-		f(address)
-	}
 }
